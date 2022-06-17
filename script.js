d3d7a55e6ce4ed6887918b3e617b2df0d52ae67f
Kateone26 <101978392+Kateone26@users.noreply.github.com>
2022-06-17 23:51:07 +0400

    js / html

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-
-
 let headerjs = document.getElementById('header');
 let togglebutton = document.getElementById('toggleburger');
 let slidenavbar = document.getElementById('slide-navbar');
 let footeroverlay = document.getElementById('footer-overlay')
 
-
 togglebutton.addEventListener('click', function(){
     headerjs.classList.add('disappear');
     slidenavbar.classList.add('appear');
-    footeroverlay.classList.add('appear')
+    footeroverlay.classList.add('appear');
+    
+
+    document.addEventListener('mouseup', function(e) {
+        var container = document.getElementById('overlay');
+        if (!container.contains(e.target)) {
+            headerjs.classList.remove('disappear');
+            slidenavbar.classList.remove('appear');
+            footeroverlay.classList.remove('appear');
+        }
+    });
+
 
 })
+
 
 
 
